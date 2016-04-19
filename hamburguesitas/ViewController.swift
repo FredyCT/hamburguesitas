13271a8c60d59c6971e710a14c2f8dbca05d7b7d
FredyCT <fct73@hotmail.com>
2016-04-19 14:28:55 -0500

    Initial Commit

---
+++ hamburguesitas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hamburguesitas
+//
+//  Created by Fredy Cervantes Téllez on 19/04/16.
+//  Copyright © 2016 CursoTecSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
